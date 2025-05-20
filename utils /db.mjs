f3b1b4b8ffa437c35518aebf6138478514a824ac
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-05-21 00:50:37 +0100

    Create db.mjs

---
+++ utils /db.mjs
@@ -0,0 +1,41 @@
+// utils/db.mjs
+import pkg from 'mongodb';
+
+const { MongoClient, ObjectId } = pkg;
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.db = null;
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch((err) => {
+        console.error('MongoDB connection error:', err);
+        this.db = null;
+      });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    if (!this.db) return 0;
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) return 0;
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
